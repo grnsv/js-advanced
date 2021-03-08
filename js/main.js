5954ae382d52f2d6eeaaa30035ce5d49190cf045
grnsv <grin.sv@gmail.com>
2021-03-08 23:32:52 +0300

    teacher's files used

--- js/main.js
+++ js/main.js
@@ -3,15 +3,15 @@ const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-a
 const app = new Vue({
     el: '#app',
     data: {
-        catalogUrl: '/catalogData.json',
-        imgCatalog: 'https://placehold.it/200x150',
-        allProducts: [],
-        products: [],
-        searchLine: '',
-        isVisibleCart: false,
+        userSearch: '',
+        showCart: false,
         cartUrl: '/getBasket.json',
-        imgCart: 'https://placehold.it/50x100',
-        cart: [],
+        catalogUrl: '/catalogData.json',
+        products: [],
+        cartItems: [],
+        filtered: [],
+        imgCatalog: 'https://placehold.it/200x150',
+        imgCart: 'https://placehold.it/50x100'
     },
     methods: {
         getJson(url){
@@ -21,55 +21,60 @@ const app = new Vue({
                     console.log(error);
                 })
         },
-        addProduct(product) {
-            console.log(product)
+        addProduct(product){
+            this.getJson(`${API}/addToBasket.json`)
+            .then(data => {
+                if(data.result === 1){
+                    let find = this.cartItems.find(el => el.id_product === product.id_product);
+                    if(find){
+                        find.quantity++;
+                    } else {
+                        let prod = Object.assign({quantity: 1}, product);
+                        this.cartItems.push(prod)
+                    }
+                } else {
+                    alert('Error');
+                }
+            })
         },
-        filterGoods() {
-            const regexp = new RegExp(this.searchLine, 'i');
-            this.products = this.allProducts.filter(product => regexp.test(product.product_name));
+        remove(item) {
+            this.getJson(`${API}/deleteFromBasket.json`)
+            .then(data => {
+                if(data.result === 1) {
+                    if(item.quantity>1){
+                        item.quantity--;
+                    } else {
+                        this.cartItems.splice(this.cartItems.indexOf(item), 1)
+                    }
+                }
+            })
+        },
+        filter(){
+            let regexp = new RegExp(this.userSearch, 'i');
+            this.filtered = this.products.filter(el => regexp.test(el.product_name));
         }
     },
-    beforeCreate() {
-        console.log('beforeCreate');
-    },
-    created() {
-        console.log('created');
-        this.getJson(`${API + this.catalogUrl}`)
-            .then(data => {
-                for(let el of data){
-                    this.allProducts.push(el);
-                }
-                this.products = this.allProducts;
-            })
-            .catch(error => {
-                console.log(error);
-            });
+    mounted(){
         this.getJson(`${API + this.cartUrl}`)
             .then(data => {
                 for(let el of data.contents){
-                    this.cart.push(el);
+                    this.cartItems.push(el);
+                }
+            });
+       this.getJson(`${API + this.catalogUrl}`)
+           .then(data => {
+               for(let el of data){
+                   this.products.push(el);
+                   this.filtered.push(el);
+               }
+           });
+        this.getJson(`getProducts.json`)
+            .then(data => {
+                for(let el of data){
+                    this.products.push(el);
+                    this.filtered.push(el);
                 }
             })
-            .catch(error => {
-                console.log(error);
-            });
-    },
-    beforeMount() {
-        console.log('beforeMount');
-    },
-    mounted() {
-        console.log('mounted');
-    },
-    beforeUpdate() {
-        console.log('beforeUpdate');
-    },
-    updated() {
-        console.log('updated');
-    },
-    beforeDestroy() {
-        console.log('beforeDestroy');
-    },
-    destroyed() {
-        console.log('beforeDestroy');
     }
-});
+})
+
